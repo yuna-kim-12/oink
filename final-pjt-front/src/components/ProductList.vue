836d12da9b8b468ec088e1544493b08e3b772bef
yunha <dbsgk45@naver.com>
2024-11-22 23:50:20 +0900

    상품 추천 상세 페이지 수정 중

--- final-pjt-front/src/components/ProductList.vue
+++ final-pjt-front/src/components/ProductList.vue
@@ -7,13 +7,19 @@
     <div class="filter-container">
       <div class="select-container">
         <select v-model="selectedBank" @change="" class="select-box">
-          <option value="">금융사</option>
+          <option value="none">상품옵션</option>
+          <option v-for="pdtoption in pdtoptions" :key="pdtoption" :value="pdtoption">
+            {{ pdtoption }}
+          </option>
+        </select>
+        <select v-model="selectedBank" @change="" class="select-box">
+          <option value="none">금융사</option>
           <option v-for="bank in banks" :key="bank" :value="bank">
             {{ bank }}
           </option>
         </select>
           <select v-model="selectedCity" @change="" class="select-box">
-            <option value="">기간</option>
+            <option value="none">기간(개월)</option>
             <option v-for="saving_period in saving_periods" :key="saving_period" :value="saving_period">
               {{ saving_period }}
             </option>
@@ -22,8 +28,8 @@
 
 
       <div class="filter-button">
-        <button>최고금리순</button> 
-        <button>기본금리순</button>
+        <button @click="setInterest(false)" :class="{ active: !interest_rate }" class="toggle-button">최고금리순</button> 
+        <button @click="setInterest(false)" :class="{ active: !interest_rate }" class="toggle-button">기본금리순</button>
       </div>
     </div>
 
@@ -52,9 +58,8 @@
                 <!-- <span>적용 금리: {{ myProduct.interest_rate }}%</span>
                 <p class="monthly">월 납입액: {{ (myProduct.monthly_amount*10000).toLocaleString(en-US)}}원</p>
                 <p class="amount">{{ (myProduct.remain_month*myProduct.monthly_amount*10000).toLocaleString(en-US) }}원</p> -->
-                <p>기준 금리: 5%</p>
-                <p>우대 금리: 5%</p>
-                <p class="amount">500,000원</p>
+                <p class="prime_interest_rate">최고 5%</p>
+                <p class="interest_rate">기본 5%</p>
             </div>
         </div>
     </div>
@@ -82,9 +87,8 @@
                 <!-- <span>적용 금리: {{ myProduct.interest_rate }}%</span>
                 <p class="monthly">월 납입액: {{ (myProduct.monthly_amount*10000).toLocaleString(en-US)}}원</p>
                 <p class="amount">{{ (myProduct.remain_month*myProduct.monthly_amount*10000).toLocaleString(en-US) }}원</p> -->
-                <p>기준 금리: 5%</p>
-                <p>우대 금리: 5%</p>
-                <p class="amount">500,000원</p>
+                <p class="prime_interest_rate">최고 5%</p>
+                <p class="interest_rate">기본 5%</p>
             </div>
         </div>
     </div>
@@ -112,9 +116,8 @@
                 <!-- <span>적용 금리: {{ myProduct.interest_rate }}%</span>
                 <p class="monthly">월 납입액: {{ (myProduct.monthly_amount*10000).toLocaleString(en-US)}}원</p>
                 <p class="amount">{{ (myProduct.remain_month*myProduct.monthly_amount*10000).toLocaleString(en-US) }}원</p> -->
-                <p>기준 금리: 5%</p>
-                <p>우대 금리: 5%</p>
-                <p class="amount">500,000원</p>
+                <p class="prime_interest_rate">최고 5%</p>
+                <p class="interest_rate">기본 5%</p>
             </div>
         </div>
     </div>
@@ -142,9 +145,8 @@
                 <!-- <span>적용 금리: {{ myProduct.interest_rate }}%</span>
                 <p class="monthly">월 납입액: {{ (myProduct.monthly_amount*10000).toLocaleString(en-US)}}원</p>
                 <p class="amount">{{ (myProduct.remain_month*myProduct.monthly_amount*10000).toLocaleString(en-US) }}원</p> -->
-                <p>기준 금리: 5%</p>
-                <p>우대 금리: 5%</p>
-                <p class="amount">500,000원</p>
+                <p class="prime_interest_rate">최고 5%</p>
+                <p class="interest_rate">기본 5%</p>
             </div>
         </div>
     </div>
@@ -154,6 +156,11 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const pdtoptions = [
+    '예금', '적금'
+  ];
 
 const banks = [
     'KB국민은행', '신한은행', '우리은행', 'NH농협은행', '하나은행', 'SC제일은행'
@@ -164,6 +171,12 @@ const saving_periods = [
 ];
 
 
+const interest_rate = ref(false); // false는 예금, true는 적금
+
+const setInterest = (value) => {
+  interest_rate.value = value;
+};
+
 </script>
 
 <style scoped>
@@ -172,7 +185,17 @@ const saving_periods = [
   margin: 100px auto;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  /* justify-content: space-between; */
+}
+
+.search-product {
+  margin-bottom: 20px;
+}
+
+.search-product > span {  
+  font-size: 20px;
+  font-weight: 700;
+  color: #565656;
 }
 
 .filter-container {
@@ -205,16 +228,6 @@ const saving_periods = [
   font-weight: 700;
   color: #808080;
   outline: none;
-}
-
-.search-product {
-  margin-bottom: 20px;
-}
-
-.search-product > span { 
-  font-size: 18px;
-  font-weight: 700;
-  color: #808080;
 }
 
 
@@ -278,7 +291,18 @@ const saving_periods = [
 .monthly-amount {
     align-self: end;
     text-align: end;
-    margin-top: 73px;
+    margin-top: 30px;
+}
+
+.monthly-amount .prime_interest_rate {
+  font-size: 24px;
+  font-weight: bold;
+  color: var(--point-color);
+}
+
+.monthly-amount .interest_rate {
+  font-size: 12px;
+  color: #999;
 }
 
 .monthly-amount > span {

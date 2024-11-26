7892353f67cbecc6b1d661009067eb51a701a4fb
yunha <dbsgk45@naver.com>
2024-11-26 16:28:47 +0900

    수정

--- final-pjt-back/accounts/urls.py
+++ final-pjt-back/accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # 회원가입 URL
-    path('register/', views.register_user),
+    # # 회원가입 URL
+    # path('register/', views.register_user),
     # 사용자 프로필 조회
     path('profile/<int:user_pk>/', views.user_profile),
     # 프로필 수정 URL
